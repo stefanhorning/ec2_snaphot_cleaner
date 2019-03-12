8d5dbfc7d2ed40e9dfab723d6b2b14e838b2fe8a
Stefan Horning <stefan@hornings.de>
2019-03-12 14:34:45 +0100

    Various improvements to script and README

--- README.md
+++ README.md
@@ -9,23 +9,24 @@ So on ubuntu for example run following commands to set things up:
 * `apt-get install awscli jq`
 * `aws configure`
 
-Also make sure you have read and write permissions for EC2.
+Also make sure you have read and write IAM permissions for EC2 (or at least for all snapshot, AMI and volume related actions).
 
 ## Usage
-Make sure script is executable (`chmod +x ec2_snaphot_cleaner.sh`). Then do a `./ec2_snaphot_cleaner.sh`
+Make sure script is executable (`chmod +x ec2_snaphot_cleaner.sh`). Then do a `./ec2_snaphot_cleaner.sh`.
 
-If you want to use a non default AWS profile (you can have multiple in your `.aws/config`) change the `AWS_PROFILE` variable
-on the top of the script. See [AWS documentation on profiles](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-multiple-profiles)
+If you want to use a non default AWS profile (you can have multiple in your `.aws/config`) set the `AWS_PROFILE` environment variable when running the script.
+Example `AWS_PROFILE=production ./ec2_snaphot_cleaner.sh`.
+See [AWS documentation on profiles](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-multiple-profiles)
 
 ## Purpose of this Tool
-This tool checks for snapshots being used as EBS volumes by machines directly using the `aws describe-volumes` command, it
+This tool checks for snapshots being used in EC2 EBS volumes using the `aws describe-volumes` command, it
 also checks snaphots being used by AMIs by using the `aws describe-images` command.
 **Snaphots that don't appear in either of those lists will be deleted!**
 
 The script will give you an output of what snaphot IDs have been found in use and which ones will be kept/deleted.
 
 ## Caution / Disclaimer / Warning!
-**Deleted snaphots can not be recovered!** So make sure first this tool actually does what you want! The `aws delete-snaphot` command (being used here) however
+**Deleted snaphots can not be recovered!** So make sure first that this tool actually does what you want! The `aws delete-snaphot` command (being used here) however
 makes sure snaphots building on top of each other (sharing blocks) stay intact. See http://docs.aws.amazon.com/cli/latest/reference/ec2/delete-snapshot.html
 
 In my usecase checking only for Volumes and AMIs in use is enough. But if you have for some reason other things depending on snaphots don't run this!
--- ec2_snaphost_cleaner.sh
+++ ec2_snaphost_cleaner.sh
@@ -3,57 +3,75 @@
 # Small tool to delete AWS EC2 Snapshots that are no longer linked to any Volumes or AMIs in use.
 # Needs jq and awscli to be installed and configured.
 # For example with `apt-get install awscli jq && aws configure`
+
+# Set the env var AWS_PROFILE to run against other AWS account then the default one
+
+# Colors:
 RED='\033[0;31m'
 NC='\033[0m' # No Color
-# set this to other profiles you might have in your aws config:
-AWS_PROFILE=default
 
-snapshots_used_by_volumes=$(aws ec2 --profile $AWS_PROFILE describe-volumes | jq -r .Volumes[].SnapshotId | sort)
-snapshots_used_by_amis=$(aws ec2 --profile $AWS_PROFILE describe-images --owner self | jq -r .Images[].BlockDeviceMappings[].Ebs.SnapshotId | sort)
+# Collecting data:
+aws_account_id=$(aws sts get-caller-identity | jq -r .Account)
+snapshots_used_by_volumes=$(aws ec2 describe-volumes | jq -r .Volumes[].SnapshotId | sort -u)
+snapshots_used_by_amis=$(aws ec2 describe-images --owner self | jq -r .Images[].BlockDeviceMappings[].Ebs.SnapshotId | sort -u)
 all_snapshots_used=()
 
 vol_count=0
-#echo "Snaphost used by EC2 Volumes"
+echo "Checking snapshots used by EC2 Volumes"
 for snap in ${snapshots_used_by_volumes[@]}; do
-#  echo "$snap"
-  all_snapshots_used+=($snap)
-  (( vol_count++ ))
+  # Make sure snapshot still extits/can be deleted (is owned by us)
+  result=$((aws ec2 describe-snapshots --snapshot-id $snap --filters Name=owner-id,Values=$aws_account_id 2> /dev/null || echo '{}') | jq .Snapshots[0])
+
+  if [ "$result" != 'null' ]; then
+    printf '.'
+    all_snapshots_used+=($snap)
+    (( vol_count++ ))
+  else
+    echo ''
+    echo "Found snapshot with id $snap in volume, but snapshot was already deleted or is not owned by this account ($aws_account_id)"
+  fi
 done
-#echo "Count: $vol_count"
+echo 'Done checking Volumes.'
+echo ''
 
 ami_count=0
-#echo "Snaphots used by EC2 AMIs"
+echo "Checking snapshots used by EC2 AMIs"
 for snap in ${snapshots_used_by_amis[@]}; do
   if [ $snap != 'null' ]; then
-#    echo "$snap"
+    printf '.'
     all_snapshots_used+=($snap)
     (( ami_count++ ))
   fi
 done
-#echo "Count: $ami_count"
-#echo "Total: $(($vol_count + $ami_count))"
+echo ''
+echo 'Done checking AMIs.'
+echo ''
 
 echo "All EC2 snapshots currently in use by either AMIs or Volumes"
 echo "------------------------------------------------------------"
-merged_count=0
+used_count=0
 all_snapshots_used=($(printf "%s\n" "${all_snapshots_used[@]}" | sort -u)) # sort and keep unique ones only
 
 for snap in ${all_snapshots_used[@]}; do
   echo "$snap"
-  (( merged_count++ ))
+  (( used_count++ ))
 done
-echo "# Count: $merged_count"
+echo "# Count: $used_count"
 echo " "
 
 echo "All EC2 snapshots currently present on AWS"
 echo "------------------------------------------"
-all_snaphots_present=$(aws ec2 --profile $AWS_PROFILE describe-snapshots --owner self | jq -r .Snapshots[].SnapshotId | sort)
+all_snaphots_present=$(aws ec2 describe-snapshots --owner self | jq -r .Snapshots[].SnapshotId | sort)
 present_count=0
 for snap in ${all_snaphots_present[@]}; do
   echo "$snap"
   (( present_count++ ))
 done
 echo "# Count: $present_count"
+echo " "
+
+echo "Will delete $((present_count - used_count)) unused snapshots in 3 seconds. Press Ctrl+C to abort."
+sleep 5
 echo " "
 
 deleted_count=0
@@ -64,7 +82,7 @@ for snap in ${all_snaphots_present[@]}; do
     echo "🗹  Keeping $snap"
   else
     printf "${RED}🗵 ${NC} Deleting $snap \n"
-    aws ec2 --profile $AWS_PROFILE delete-snapshot --snapshot-id $snap
+    aws ec2 delete-snapshot --snapshot-id $snap
     (( deleted_count++ ))
   fi
 done
